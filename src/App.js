1c03e98af02cc12d2c23de1de6bd727883b8e3e5
Chanon <5910110071@psu.ac.th>
2018-09-17 11:23:10 +0700

    การทดลอง Components and Props

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
 
+const word = "Hello";
 class App extends Component {
-  x="Hello {''World''}"
-  render() {
-    return (
-      <div className="App">
-        <CharacterCard value="h"/>
-        <CharacterCard value="i"/>
-      </div>
-    );
-  }
+ render() {
+ return (
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
+ </div>
+ );
+ }
 }
 
 export default App;
