0ecc57c6948687546b9f91cd02c8372e22e0b5bf
Chanon <5910110071@psu.ac.th>
2018-09-17 11:56:37 +0700

    More

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
-
+import WordCard from './CharacterCard';
 const word = "Hello";
 class App extends Component {
  render() {
- return (
- <div>
- {
- Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
- }
- </div>
- );
- }
+  return (
+    <div className = "card">
+      <WordCard value="hello"/>
+    </div>
+    );
+   }
 }
 
 export default App;
