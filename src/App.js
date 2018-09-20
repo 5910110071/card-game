3b6bcf438bcb199e38e5da20c29f5b86e4373d1e
Chanon <5910110071@psu.ac.th>
2018-09-21 01:14:29 +0700

    Components & Props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import CharacterCard from './CharacterCard';
-import WordCard from './CharacterCard';
+//import CharacterCard from './CharacterCard';
+import WordCard from "./WordCard";
 const word = "Hello";
 class App extends Component {
- render() {
-  return (
-    <div className = "card">
-      <WordCard value="hello555555555555555555555"/>
-    </div>
+  
+  render() {
+    return (
+      <div>
+        {
+          <WordCard value="hello"/>
+        }
+        
+      </div>
     );
-   }
+  }
 }
 
 export default App;
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,11 +1,13 @@
-import CharacterCard from './CharacterCard';
-export default class WordCard extends
-Component {
+import CharacterCard from "./CharacterCard";
+import React, { Component }from 'react';
+import './App.css';
+export default class WordCard extends Component{
+    activationHandler = c => { console.log(`${c} has been activated.`)}
 render() {
-    return (
-        <div>
-            { Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
-        </div>
-        );
-    }
+ return (
+ <div>
+{ Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
+ </div>
+ );
+ }
 }
