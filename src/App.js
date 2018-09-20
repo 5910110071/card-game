d0a09d9cdab8a3995520b3fe961053043268dfba
Chanon <5910110071@psu.ac.th>
2018-09-21 00:04:11 +0700

    test2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
  render() {
   return (
     <div className = "card">
-      <WordCard value="hello555555555"/>
+      <WordCard value="hello555555555555555555555"/>
     </div>
     );
    }
