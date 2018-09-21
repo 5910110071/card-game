a4ace84d762821667b3590c6efea87f6c56e46df
Chanon <5910110071@psu.ac.th>
2018-09-21 21:04:14 +0700

    ตอนที่ เพิ่มคำใบ้

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import WordCard from './WordCard';
+import ChanonQuestion from './ChanonQuestion';
 
 const word = "rainbow";
+const Quest = "Engineering";
 class App extends Component{
   render(){
     return(
       <div>
         <WordCard value={word}/>
+        <ChanonQuestion value={Quest}/>
       </div>
     );
   }
